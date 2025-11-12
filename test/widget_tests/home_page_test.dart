5496734430d9958954d4cce8c2c0e0800363cb54
Irwin <carpool7200@gmail.com>
2025-11-11 19:06:46 -0500

    added in home_page widget test. Need to figure out why it is failing

---
+++ test/widget_tests/home_page_test.dart
@@ -0,0 +1,59 @@
+import 'package:find_my_pickle/view/home_page.dart';
+import 'package:find_my_pickle/view/login.dart';
+import 'package:find_my_pickle/view/search.dart';
+import 'package:find_my_pickle/view/signup.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+Widget createHomePage() {
+  return const MaterialApp(
+    home: HomePage(),
+  );
+}
+
+void main() {
+  group('Home page widget test)', () {
+    
+    testWidgets('HomePage renders all components correctly', (tester) async {
+      await tester.pumpWidget(createHomePage());
+      await tester.pump(Duration.zero);
+
+      //Verify all components are on screen
+      expect(find.text('Find My 🥒'), findsOneWidget);
+      expect(find.text('Login'), findsOneWidget);
+      expect(find.text('Sign Up'), findsOneWidget);
+      expect(find.text('Continue As Guest'), findsOneWidget);
+      expect(find.byType(Image), findsOneWidget);
+    });
+
+    testWidgets('Tapping Login button navigates to login', (tester) async {
+      await tester.pumpWidget(createHomePage());
+
+      await tester.tap(find.text('Login'));
+      await tester.pumpAndSettle();
+
+      expect(find.byType(HomePage), findsNothing);
+      expect(find.byType(Login), findsOneWidget);
+    });
+
+    testWidgets('Tapping Sign Up button navigates to sign up', (tester) async {
+      await tester.pumpWidget(createHomePage());
+      
+      await tester.tap(find.text('Sign Up'));
+      await tester.pumpAndSettle();
+
+      expect(find.byType(HomePage), findsNothing);
+      expect(find.byType(Signup), findsOneWidget);
+    });
+
+    testWidgets('Tapping Continue As Guest navigates to search', (tester) async {
+      await tester.pumpWidget(createHomePage());
+      
+      await tester.tap(find.text('Continue As Guest'));
+      await tester.pumpAndSettle();
+
+      expect(find.byType(HomePage), findsNothing);
+      expect(find.byType(SearchPage), findsOneWidget);
+    });
+  });
+}
